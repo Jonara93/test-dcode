1307c6ed7c139774289631c5e4e2b087bc6e912b
Jonathan <arabia.jonathan@hotmail.be>
2021-05-03 07:58:44 +0200

    Add @CrossOrigin

--- src/main/java/eu/dcode/ecommerce/controller/ProductController.java
+++ src/main/java/eu/dcode/ecommerce/controller/ProductController.java
@@ -2,6 +2,7 @@ package eu.dcode.ecommerce.controller;
 
 import eu.dcode.ecommerce.model.Product;
 import eu.dcode.ecommerce.service.ProductService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.constraints.NotNull;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/products")
 public class ProductController {
 
